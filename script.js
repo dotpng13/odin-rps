6f8eb2b15efbe1ea182af97bff362d2af18428f9
Quinn Siebers <packerfan21396@gmail.com>
2023-03-31 17:45:01 -0500

    Remove debugging console logs in playRound
    Remove unnecessary console.log statments in playRound that were a part
    of the debugging process.

--- script.js
+++ script.js
@@ -17,42 +17,23 @@ function getComputerChoice() {
 function playRound (playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
 
-    console.log(playerSelection + ", " + computerSelection)
-
     let win = `You Win! ${playerSelection} beats ${computerSelection}`;
     let lose = `You Lose! ${computerSelection} beats ${playerSelection}`;
 
     switch (true) {
         case (playerSelection === "rock") && (computerSelection === "paper"):
-            console.log("rp: " + playerSelection + ", " + computerSelection)
             return lose;
         case (playerSelection === "rock") && (computerSelection === "scissors"):
-            console.log("rs: " + playerSelection + ", " + computerSelection)
             return win;
         case (playerSelection === "paper") && (computerSelection === "rock"):
-            console.log("pr: " + playerSelection + ", " + computerSelection)
             return win;
         case (playerSelection === "paper") && (computerSelection === "scissors"):
-            console.log("ps: " + playerSelection + ", " + computerSelection)
             return lose;
         case (playerSelection === "scissors") && (computerSelection === "rock"):
-            console.log("sr: " + playerSelection + ", " + computerSelection)
             return lose;
         case (playerSelection === "scissors") && (computerSelection === "paper"):
-            console.log("sp: " + playerSelection + ", " + computerSelection)
             return win;
         default:
-            console.log("d: " + playerSelection + ", " + computerSelection)
             return `You Tie! ${playerSelection} was both selected`;
     }
 }
-
-console.log(playRound("ROCK", "rock"))
-console.log(playRound("roCK", "paper"))
-console.log(playRound("rock", "scissors"))
-console.log(playRound("paper", "rock"))
-console.log(playRound("paper", "paper"))
-console.log(playRound("paper", "scissors"))
-console.log(playRound("scissors", "rock"))
-console.log(playRound("scissors", "paper"))
-console.log(playRound("scissors", "scissors"))
